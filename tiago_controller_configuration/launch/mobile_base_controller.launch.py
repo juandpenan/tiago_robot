05b9180ab8a7fd72bffdf6ef336d4e15c312dc87
Sergi Garcia <sergi.garcia@pal-robotics.com>
2024-03-06 16:15:18 +0100

    Parameterized mobile base type

--- tiago_controller_configuration/launch/mobile_base_controller.launch.py
+++ tiago_controller_configuration/launch/mobile_base_controller.launch.py
@@ -15,11 +15,13 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 from controller_manager.launch_utils import generate_load_controller_launch_description
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch import LaunchDescription, LaunchContext
+from dataclasses import dataclass
 
 
 def controller_bringup(context, *args, **kwargs):
@@ -35,9 +37,15 @@ def controller_bringup(context, *args, **kwargs):
             get_package_share_directory('tiago_controller_configuration'),
             'config', 'mobile_base_controller.yaml')
     load_controller_arg = generate_load_controller_launch_description(
-        controller_name='mobile_base_controller',
-        controller_type='omni_drive_controller/OmniDriveController',
-        controller_params_file=os.path.join(
+
+def setup_controller_configuration(context: LaunchContext):
+    actions = []
+    base_type = read_launch_argument('base_type', context)
+    if base_type == "omni_base":
+        launch_controller = generate_load_controller_launch_description(
+          controller_name="mobile_base_controller",
+          controller_type='omni_drive_controller/OmniDriveController',
+          controller_params_file=os.path.join(
 
     actions.append(load_controller_arg)
     return actions
@@ -55,5 +63,23 @@ def generate_launch_description():
     ld.add_action(is_public_sim_arg)
     controller_bringup_launch = OpaqueFunction(function=controller_bringup)
     ld.add_action(controller_bringup_launch)
+
+    return ld
+            controller_type='diff_drive_controller/DiffDriveController',
+            controller_params_file=params_file)
+        actions.append(launch_controller)
+        return actions
+
+
+def generate_launch_description():
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    launch_arguments = LaunchArguments()
+
+    launch_arguments.add_to_launch_description(ld)
+
+    declare_actions(ld, launch_arguments)
 
     return ld
