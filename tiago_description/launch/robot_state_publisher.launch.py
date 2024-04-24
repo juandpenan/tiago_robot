44bfbb1edf833a81b0c8fd53ad131d83b8b3c672
Oscar <oscarmartinez@pal-robotics.com>
2024-04-24 16:38:42 +0200

    Final mods

--- tiago_description/launch/robot_state_publisher.launch.py
+++ tiago_description/launch/robot_state_publisher.launch.py
@@ -104,7 +104,7 @@ def create_robot_description_param(context, *args, **kwargs):
         "laser_model": read_launch_argument("laser_model", context),
         "wrist_model": read_launch_argument("wrist_model", context),
         "base_type": read_launch_argument("base_type", context),
-        "use_sim": read_launch_argument("use_sim_time", context),
+        "use_sim_time": read_launch_argument("use_sim_time", context),
         "namespace": read_launch_argument("namespace", context),
         "has_screen": read_launch_argument("has_screen", context)
     }
